2996f2d0c1f86ab9fc2b3a04386336f16aae8ddb
Robert Dickerson <rdickeriii@gmail.com>
2017-10-27 20:04:05 -0700

    Refactor: signaling to mobx dynamic values
    end_of_line

--- sandbox-mobx.js
+++ sandbox-mobx.js
@@ -1,26 +1,32 @@
-class TodoStore {
-	todos = [];
+class ObservableTodoStore {
+	@observable todos = [];
+    @observable pendingRequests = 0;
 
-	get completedTodosCount() {
+    constructor() {
+        mobx.autorun(() => console.log(this.report));
+    };
+
+	@computed get completedTodosCount() {
     	return this.todos.filter(
 			todo => todo.completed === true
 		).length;
     }
 
-	report() {
+	@computed get report() {
 		if (this.todos.length === 0)
 			return "<none>";
 		return `Next todo: "${this.todos[0].task}". ` +
 			`Progress: ${this.completedTodosCount}/${this.todos.length}`;
 	}
 
-    addTodo(task) {
+	addTodo(task) {
 		this.todos.push({
 			task: task,
 			completed: false,
-            assignee: null
+			assignee: null
 		});
 	}
 }
 
-const todoStore = new TodoStore();
+
+const observableTodoStore = new ObservableTodoStore();
